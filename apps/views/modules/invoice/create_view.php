087aa1535c585f864ec0806d53742aebe761f877
ArrieW <arrwebz@gmail.com>
2025-09-22 13:01:56 +0700

    css btn

--- apps/views/modules/invoice/create_view.php
+++ apps/views/modules/invoice/create_view.php
@@ -227,13 +227,13 @@
                                 <div id="boxNet" class="hidden">
                                     <p>Hitung NET:</p>
                                     <span id="valueNet8"></span>
-                                    <button type="button" id="btnAddNet8">Use -8%</button>
+                                    <button type="button" id="btnAddNet8" class="btn btn-xs btn-light rounded-pill px-4 mb-6 waves-effect waves-light">Use -8%</button>
                                     <span id="valueNet10"></span>
-                                    <button type="button" id="btnAddNet10">Use -10%</button>
+                                    <button type="button" id="btnAddNet10" class="btn btn-xs btn-light rounded-pill px-4 mb-6 waves-effect waves-light">Use -10%</button>
                                     <span id="valueNet12"></span>
-                                    <button type="button" id="btnAddNet12">Use -12%</button>
+                                    <button type="button" id="btnAddNet12" class="btn btn-xs btn-light rounded-pill px-4 mb-6 waves-effect waves-light">Use -12%</button>
                                     <span id="valueNet15"></span>
-                                    <button type="button" id="btnAddNet15">Use -15%</button>
+                                    <button type="button" id="btnAddNet15" class="btn btn-xs btn-light rounded-pill px-4 mb-6 waves-effect waves-light">Use -15%</button>
                                 </div>
                                 <div class="input-group">
                                     <span class="input-group-text">Rp</span>
@@ -248,9 +248,9 @@
                                 <div id="boxPPN" class="hidden">
                                     <p>Hitung PPN:</p>
                                     <span id="valueAutoPpn11"></span>
-                                    <button type="button" id="btnAddPpn11">Use +11%</button>
+                                    <button type="button" id="btnAddPpn11" class="btn btn-xs btn-light rounded-pill px-4 mb-6 waves-effect waves-light">Use +11%</button>
                                     <span id="valueAutoPpn12"></span>
-                                    <button type="button" id="btnAddPpn12">Use +12%</button>
+                                    <button type="button" id="btnAddPpn12" class="btn btn-xs btn-light rounded-pill px-4 mb-6 waves-effect waves-light">Use +12%</button>
                                 </div>
                                 <div class="input-group">
                                     <span class="input-group-text">Rp</span>
@@ -283,7 +283,7 @@
                         <input type="number" name="qty" placeholder="Qty">
                         <input type="text" name="unit" placeholder="Unit">
                         <input type="number" name="price" placeholder="Price">
-                        <button type="button" id="btnAddItem" class="btn mb-1 bg-success btn-circle btn-md d-inline-flex align-items-center justify-content-center">Add Item</button>
+                        <button type="button" id="btnAddItem" class="btn mb-1 bg-success btn-circle btn-xs d-inline-flex align-items-center justify-content-center">Add Item</button>
                     </form>
                     <table id="itemTable" class="table">
                         <thead>
